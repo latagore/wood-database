041e1408de2746fd6ea1b66c9d13d63239bfbbd6
Steven Xu <s@stevenxu.xyz>
2017-05-24 14:28:57 -0400

    create wood objects and save name+links

--- index.js
+++ index.js
@@ -9,8 +9,18 @@ request(HOME_PAGE_URL, (error, response, html) => {
   // selectors for the left and right column, copy-pasted from Chrome DevTools
   const PARENT_SELECTOR = '#post-7 > div > div.fusion-fullwidth.fullwidth-box.hundred-percent-fullwidth > div > div:nth-child(4) > div, #post-7 > div > div.fusion-fullwidth.fullwidth-box.hundred-percent-fullwidth > div > div:nth-child(5) > div';
   
-  let links = $(PARENT_SELECTOR).find("a");
+  let links = $(PARENT_SELECTOR).find("h3 a");
+  let woods = [];
   for(let i = 0; i < links.length; i++) {
-    console.log(links[i].attribs.href);
+    let link = links.eq(i);
+    console.log(`processing ${link.text()}`);
+    let wood = {
+      name: link.text(),
+      page: link.attr('href'),
+      thumbnail: undefined // TODO
+    };
+    woods.push(wood);
   }
+  
+  console.log("done!");
 });
