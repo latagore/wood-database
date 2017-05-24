7f568afdcb05e8c406aba1ee458d4583aa0bce55
Steven Xu <s@stevenxu.xyz>
2017-05-24 14:35:20 -0400

    use request-promise instead of request

--- index.js
+++ index.js
@@ -1,26 +1,35 @@
-let request = require('request');
+let rp = require('request-promise');
 let cheerio = require('cheerio');
 
 const HOME_PAGE_URL = 'http://www.wood-database.com';
-request(HOME_PAGE_URL, (error, response, html) => {
-  if (error) return;
-  
-  let $ = cheerio.load(html);
-  // selectors for the left and right column, copy-pasted from Chrome DevTools
-  const PARENT_SELECTOR = '#post-7 > div > div.fusion-fullwidth.fullwidth-box.hundred-percent-fullwidth > div > div:nth-child(4) > div, #post-7 > div > div.fusion-fullwidth.fullwidth-box.hundred-percent-fullwidth > div > div:nth-child(5) > div';
-  
-  let links = $(PARENT_SELECTOR).find("h3 a");
-  let woods = [];
-  for(let i = 0; i < links.length; i++) {
-    let link = links.eq(i);
-    console.log(`processing ${link.text()}`);
-    let wood = {
-      name: link.text(),
-      page: link.attr('href'),
-      thumbnail: undefined // TODO
-    };
-    woods.push(wood);
+let options = {
+  uri: HOME_PAGE_URL,
+  transform: function (body) {
+    return cheerio.load(body);
   }
-  
-  console.log("done!");
-});
+}
+
+rp(options)
+  .then(($) => {
+    // selectors for the left and right column, copy-pasted from Chrome DevTools
+    const PARENT_SELECTOR = '#post-7 > div > div.fusion-fullwidth.fullwidth-box.hundred-percent-fullwidth > div > div:nth-child(4) > div, #post-7 > div > div.fusion-fullwidth.fullwidth-box.hundred-percent-fullwidth > div > div:nth-child(5) > div';
+
+    let links = $(PARENT_SELECTOR).find("h3 a");
+    let woods = [];
+    for(let i = 0; i < links.length; i++) {
+      let link = links.eq(i);
+      console.log(`processing ${link.text()}`);
+      let wood = {
+        name: link.text(),
+        page: link.attr('href'),
+        thumbnail: undefined // TODO
+      };
+      woods.push(wood);
+    }
+
+    console.log("done!");
+  })
+  .catch((err) => {
+    console.log("something went wrong");
+    console.log(err);
+  });
